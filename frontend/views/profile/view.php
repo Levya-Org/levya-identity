97c5cb34ce58bc7d5444536bdd835dd55f125dc1
Herve-M <herve.matysiak@viacesi.fr>
2015-11-02 15:21:52 +0100

    Add URL to Service & getServiceByUser

--- frontend/views/profile/view.php
+++ frontend/views/profile/view.php
@@ -2,7 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\grid\GridView;
+use yii\bootstrap\Tabs;
 use common\helpers\RoleHelper;
+
+use common\models\Service;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
@@ -18,33 +22,60 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a(Yii::t('app/user', 'Update'), ['update'], ['class' => 'btn btn-primary']) ?>
     </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'attribute'=> 'USER_LASTNAME',
-                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
+    
+    <?=
+        Tabs::widget([
+            'items' => [
+                [
+                    'label'=>'<i class="glyphicon glyphicon-user"></i> Informations',
+                    'content' => DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            [
+                                'attribute'=> 'USER_LASTNAME',
+                                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
+                            ],
+                            [
+                                'attribute'=> 'USER_FORNAME',
+                                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
+                            ],
+                            'USER_MAIL',
+                            'USER_NICKNAME', 
+                            [
+                                'attribute'=> 'USER_ADDRESS',
+                                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
+                            ],
+                            'r_Country.COUNTRY_NAME',
+                            [
+                                'attribute'=> 'USER_PHONE',
+                                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
+                            ],            
+                            'USER_LDAPUID',
+                            'USER_CREATIONDATE:datetime',
+                            'USER_REGISTRATIONDATE:datetime',
+                        ],
+                    ])
+                ],
+                [
+                    'label'=>'<i class="glyphicon glyphicon-th-list"></i> Services',
+                    'content' => GridView::widget([
+                        'dataProvider' => new yii\data\ArrayDataProvider([
+                            'allModels' => Service::getServicesByUser($model->USER_ID),
+                        ]), 
+                        'columns' => [
+                            'SERVICE_NAME',
+                            'SERVICE_URL:url',
+                            'SERVICE_ISENABLE:boolean',
+                            'SERVICE_STATE:boolean',                        
+                        ],
+                    ])
+                ],
+                [
+                    'label'=>'<i class="glyphicon glyphicon-briefcase"></i> Projects',
+                    'headerOptions' => ['class'=>'disabled']
+                ],
             ],
-            [
-                'attribute'=> 'USER_FORNAME',
-                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
-            ],
-            'USER_MAIL',
-            'USER_NICKNAME', 
-            [
-                'attribute'=> 'USER_ADDRESS',
-                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
-            ],
-            'r_Country.COUNTRY_NAME',
-            [
-                'attribute'=> 'USER_PHONE',
-                'visible'=> RoleHelper::userHasRole(Yii::$app->user->id, RoleHelper::ROLE_MEMBER),
-            ],            
-            'USER_LDAPUID',
-            'USER_CREATIONDATE:datetime',
-            'USER_REGISTRATIONDATE:datetime',
-        ],
-    ]) ?>
-
+            'encodeLabels' => false
+        ]) 
+    ?>
 </div>
