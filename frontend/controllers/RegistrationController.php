0f67b688e8500d208d33d76f259a7c3c87828b71
Herve-M <herve.matysiak@viacesi.fr>
2015-11-02 18:37:49 +0100

    Add/Update license header for Levya Org.

--- frontend/controllers/RegistrationController.php
+++ frontend/controllers/RegistrationController.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * This file is part of Levya Identity.
+ * 
+ * Levya Identity is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. See file LICENSE(.md) in this source tree, 
+ * if not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) Levya Team Members
+ */
+
 namespace frontend\controllers;
 
 use yii\web\Controller;
